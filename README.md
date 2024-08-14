7f0856e51312d357c64d4d26326f5cf2964ee19c
Aniket Sedhai <aniketsedhai@gmail.com>
2024-08-14 14:35:06 -0500

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-/* Add Author and Project Details here */
+Author: Aniket Sedhai
+Project: Course 1 Module 1 of Introduction to Embedded Systems Software and Development Environments by University of Colorado Boulder
--- stats.c
+++ stats.c
@@ -40,54 +40,199 @@ void main() {
   /* Other Variable Declarations Go Here */
   /* Statistics and Printing Functions Go Here */
 
+  print_array(test, SIZE);
+  
+  sort_array(test, SIZE);
+
+  print_array(test, SIZE);
+
+  print_statistics (test, SIZE);
 }
 /*********************************************************************************************************************/
 
 void print_array(unsigned char* array_p, uint8_t array_length)
 {
+  /*check for null pointer*/
+  if (array_p == NULL)
+  {
+    printf("null pointer detected\n");
+    return;
+  }
 
+  if (array_length < 1)
+  {
+    printf("invalid input array size\n");
+    return;
+  }
+
+  printf("Array: ");
+
+  for (uint8_t i = 0; i < array_length - 1; ++i)
+  {
+    printf("%d, ", array_p[i]);
+  }
+
+  printf("%d\n", array_p[array_length - 1]);
 }
 
 /*********************************************************************************************************************/
 
 void print_statistics(unsigned char* array_p, uint8_t array_length)
 {
-
+  printf("==== Statistics ====\n");
+  printf("Minimum: %d\n", find_minimum(array_p, array_length));
+  printf("Maximum: %d\n", find_maximum(array_p, array_length));
+  printf("Mean: %.3f\n", find_mean(array_p, array_length));
+  printf("Median: %.3f\n", find_median(array_p, array_length));
+  printf("====================\n");
 }
 
 /*********************************************************************************************************************/
 
 double find_median(unsigned char* array_p, uint8_t array_length)
 {
+  /*check for null pointer*/
+  if (array_p == NULL)
+  {
+    printf("null pointer detected\n");
+    return -1;
+  }
+
+  if (array_length < 1)
+  {
+    printf("invalid input array size\n");
+    return -1;
+  }
+
+  /*sort the array*/
+  sort_array(array_p, array_length);
+
+  /*find the median*/
+  if (array_length % 2 == 0)
+  {
+     return (double)(array_p[(array_length / 2) - 1] + array_p[(array_length / 2)]) / 2.0;
+  }
+  else
+  {
+    return (double) array_p[(array_length + 1) / 2];
+  }
+
   return -1;
+}
+ 
+/*********************************************************************************************************************/
+
+void merge(unsigned char* array_p, uint8_t left_index, uint8_t mid_index, uint8_t right_index)
+{
+  uint8_t i, j, k;
+  uint8_t left_array_size = mid_index - left_index + 1;
+  uint8_t right_array_size = right_index - mid_index;
+
+  //create temp arrays
+  unsigned char left_array[left_array_size], right_array[right_array_size];
+
+  for (i = 0; i < left_array_size; ++i)
+  {
+    left_array[i] = array_p[left_index + i];
+  }
+
+  for (j = 0; j < right_array_size; ++j)
+  {
+    right_array[j] = array_p[mid_index + 1 + j];
+  }
+
+  //Merge the temp arrays back
+  //reset the index of the first and the second sub array
+  i = 0; j = 0;
+
+  //initial index of the merged subarray
+  k = left_index;
+
+  while ((i < left_array_size) && (j < right_array_size))
+  {
+    if (left_array[i] >= right_array[j])
+    {
+      array_p[k] = left_array[i];
+      ++i;
+    }
+    else
+    {
+      array_p[k] = right_array[j];
+      ++j;
+    }
+
+    ++k;
+  }
+
+  //copy the remaining elements of left array if any
+  while (i < left_array_size)
+  {
+    array_p[k] = left_array[i];
+    ++i;
+    ++k;
+  }
+
+  //copy the remaining elements of right array if any
+  while (j < right_array_size)
+  {
+    array_p[k] = right_array[j];
+    ++j;
+    ++k;
+  }
+}
+
+/*********************************************************************************************************************/
+
+void merge_sort(unsigned char* array_p, uint8_t left_index, uint8_t right_index)
+{
+  if (left_index < right_index)
+  {
+    uint8_t mid_index = left_index + (right_index - left_index) / 2;
+
+    merge_sort(array_p, left_index, mid_index);
+    merge_sort(array_p, mid_index + 1, right_index);
+
+    merge(array_p, left_index, mid_index, right_index);
+  }
 }
 
 /*********************************************************************************************************************/
 
 void sort_array(unsigned char* array_p, uint8_t array_length)
 {
-  return;
+  merge_sort(array_p, 0, array_length - 1);
 }
 
 /*********************************************************************************************************************/
 
 double find_mean(unsigned char* array_p, uint8_t array_length)
 {
-  return  0.0;
+  uint32_t sum = 0;
+
+  for (uint8_t i = 0; i < array_length; ++i)
+  {
+    sum += array_p[i];
+  }
+
+  return  (double) sum / (double) array_length;
 }
 
 /*********************************************************************************************************************/
 
 unsigned char find_minimum(unsigned char* array_p, uint8_t array_length)
 {
-  return 0;
+  sort_array(array_p, array_length);
+
+  return (array_p[array_length - 1]);
 }
 
 /*********************************************************************************************************************/
 
 unsigned char find_maximum(unsigned char* array_p, uint8_t array_length)
 {
-  return 0;
+  sort_array(array_p, array_length);
+
+  return (array_p[0]);
 }
 
 /*********************************************************************************************************************/
